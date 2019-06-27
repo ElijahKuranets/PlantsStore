ba991f22bd592ac6657d39b817fd04599a53247c
Elijah <elijah.kuranets@p97.com>
2019-06-27 16:08:33 +0300

    Fixed URL (?page=2 to Page2)

--- PlantStore.WebUI/App_Start/RouteConfig.cs
+++ PlantStore.WebUI/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace PlantStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Plant", action = "List" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Plant", action = "List", id = UrlParameter.Optional }
